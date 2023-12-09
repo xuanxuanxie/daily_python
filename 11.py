d8dc3c25ab3f0c8d1df4092df67ab2bc6b33a8d7
xuanxuanxie <86835621+xuanxuanxie@users.noreply.github.com>
2023-12-09 21:16:50 +0800

    //

--- 11.py
+++ 11.py
@@ -2,23 +2,23 @@ import os
 import re
 from shutil import copy
 
-path = "C:\\Users\\mhzxx\\Desktop\\domain\\domain1_bandpass_data"
+path = "C:\\Users\\mhzxx\\Desktop\\domain\\domain1_bandpass_data"     #the file path 
 list = os.listdir(path)
 
 new_list = []
 
 for i in list:
-    if re.match('.*lowpass_magnitude',i) != None:
+    if re.match('.*lowpass_magnitude',i) != None:     #can change the match words
         new_list.append(i)
 
 
 for data in new_list:
     a = os.path.split(path)
-    save_dir = a[0]+"\\fudu"
+    save_dir = a[0]+"\\fudu"          #path to save the result
     ori_path = path+"\\"+data
     copy(ori_path,save_dir)
 
-print("ok")
+print("ok")   
 
 
 
